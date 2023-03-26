f34fab3db1de759c3c3e186ab2f8ded9f835380c
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-03-26 12:34:30 +0530

    M

---
+++ Graphs/Detect cycle in an undirected graph(DFS).cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+     bool dfs(int node, int parent, int vis[], vector<int> adj[]){
+        vis[node]=1;
+        for(auto adjNode:adj[node]){
+            if(!vis[adjNode]){
+                if(dfs(adjNode, node, vis, adj)==true) return true;
+            }
+            else if(adjNode!=parent) return true;
+        }
+        return false;
+    }
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        int vis[V]={0};
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(i,-1,vis,adj)==true) return true;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
