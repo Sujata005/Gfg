65e22a984702334eebc4c92b915871fa9e3a2d70
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-06-06 10:01:29 +0530

    Medium

--- Graphs/Minimum Spanning Tree.cpp
+++ Graphs/Minimum Spanning Tree.cpp
@@ -50,6 +50,33 @@ public:
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
+	    
+	    
+	    // prim's algo
+	    /*
+	    
+	    priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int>>> pq;
+        vector<int> vis(V,0);
+        int sum=0;
+        pq.push({0,0});     //(weight,node)
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int node=it.second;
+            int wt=it.first;
+            if(vis[node]==1) continue;
+            vis[node]=1;
+            sum+=wt;
+            for(auto it:adj[node]){
+                int adjNode=it[0];
+                int edW=it[1];
+                if(!vis[adjNode]){
+                    pq.push({edW,adjNode});
+                }
+            }
+        }
+        return sum;
+	*/
     }
 };
 class Solution
